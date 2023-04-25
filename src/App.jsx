314ebbf90a4c56231670f37806c400a90f011efd
Ram Goel <rgoel766@gmail.com>
2023-04-25 16:26:04 +0530

    lint errors fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import './App.css'
-import Community from './pages/Community'
 import Projects from './pages/Projects'
-import Experience from './pages/Experience'
 import Loader from './pages/Loader/Loader'
-import Home from './pages/Home'
-import Why from './pages/Why'
-import FullTime from './pages/FullTime'
-import Footer from './pages/Footer'
-import Choose from './pages/Choose'
+// import Community from './pages/Community'
+// import Experience from './pages/Experience'
+// import Home from './pages/Home'
+// import Why from './pages/Why'
+// import FullTime from './pages/FullTime'
+// import Footer from './pages/Footer'
+// import Choose from './pages/Choose'
 function App() {
 
     const [loaded, setLoaded] = useState(true)
